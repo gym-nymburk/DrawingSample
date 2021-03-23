09f767c8880df2f2090bb7b86e09c412b2618f79
Tomáš Berný <tomas.berny@pragmatik.cz>
2021-03-23 13:38:11 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDraw.ForeColor = System.Drawing.Color.Red;
+            this.btnDraw.Location = new System.Drawing.Point(747, 445);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(129, 40);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Refresh";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlCanvas.BackColor = System.Drawing.Color.White;
+            this.pnlCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlCanvas.Location = new System.Drawing.Point(20, 16);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(515, 469);
+            this.pnlCanvas.TabIndex = 1;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(555, 16);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(321, 69);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(678, 65);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(52, 20);
+            this.lblRadius.TabIndex = 3;
+            this.lblRadius.Text = "radius";
+            this.lblRadius.Click += new System.EventHandler(this.lblRadius_Click);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.propertyGrid1.HelpVisible = false;
+            this.propertyGrid1.Location = new System.Drawing.Point(555, 106);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(321, 333);
+            this.propertyGrid1.TabIndex = 4;
+            this.propertyGrid1.ToolbarVisible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 505);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.lblRadius);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pnlCanvas);
+            this.Controls.Add(this.btnDraw);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 400);
+            this.Name = "Form1";
+            this.Text = "Ukázka kreslení pomocí System.Drawing";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Panel pnlCanvas;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            setting = new Setting();
+
+            propertyGrid1.SelectedObject = setting;
+
+            Draw(setting);
+        }
+
+
+        private Setting setting;
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Draw(setting);   
+        }
+
+
+        private void DrawCircle(Graphics g, Pen pen, Point center, int radius)
+        {
+            g.DrawEllipse(pen, center.X - radius, center.Y - radius, 2 * radius, 2 * radius);
+        }
+
+        private void Draw(Setting setting)
+        {
+            Bitmap bmp = new Bitmap(pnlCanvas.Width, pnlCanvas.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Pen pen = new Pen(setting.Color, setting.Width);
+
+            Point center = new Point(pnlCanvas.Width / 2, pnlCanvas.Height / 2);
+
+            DrawCircle(g, pen, center, setting.Radius);
+
+            pnlCanvas.BackgroundImage = bmp;
+
+            lblRadius.Text = $"radius: {setting.Radius}";
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            setting.Radius = trackBar1.Value;
+            Draw(setting);
+        }
+
+        private void lblRadius_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Setting
+    {
+        public Color Color { get; set; } = Color.Red;
+
+        public int Width { get; set; } = 2;
+
+        public int Radius { get; set; } = 100;
+    }
+}
